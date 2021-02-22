e66ecc8bdcb1867685822b47f4cff5af9d705991
Maya Santiago <msatori.code@gmail.com>
2021-02-22 09:55:57 -0600

    signup not working

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
-import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 
 const client = new ApolloClient({
   request: (operation) => {
@@ -13,28 +14,29 @@ const client = new ApolloClient({
 
     operation.setContext({
       headers: {
-        authorization: token ? ` Bearer ${token}` : '',
-      }
+        authorization: token ? `Bearer ${token}` : '',
+      },
     });
   },
-  uri: '/graphql'
-})
+  uri: '/graphql',
+});
 
 function App() {
   return (
     <ApolloProvider client={client}>
-    <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
-    </Router>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={SearchBooks} />
+            <Route exact path='/saved' component={SavedBooks} />
+            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
     </ApolloProvider>
   );
 }
 
 export default App;
+
