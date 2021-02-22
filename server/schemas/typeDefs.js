480772d78ec31f90589b799bd72e573711918052
Maya Santiago <msatori.code@gmail.com>
2021-02-22 11:05:41 -0600

    kl

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,18 +2,18 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql `
 
+type User {
+    _id: ID!
+    username: String!
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+}
     type Auth {
         token: ID!
         user: User
     }
 
-    type User {
-        _id: ID!
-        username: String!
-        email: String
-        bookCount: Int
-        savedBooks: [Book]
-    }
 
     type Book {
         bookId: ID!
